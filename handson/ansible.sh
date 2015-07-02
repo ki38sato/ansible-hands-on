d2927f3789508b5f2310573471e29b975d3afa98
Kimiya Sato <kimiya.sato@bizreach.co.jp>
2015-07-02 16:08:37 +0900

    upgrade epel.repo settings

--- handson/ansible.sh
+++ handson/ansible.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+sudo cp /vagrant/epel.repo /etc/yum.repos.d/epel.repo
 sudo yum -y install ansible
 sudo yum -y install libselinux-python
 mv /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg_org
--- localdev/ansible_local.sh
+++ localdev/ansible_local.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+sudo cp /vagrant/epel.repo /etc/yum.repos.d/epel.repo
 sudo yum -y install ansible
 sudo yum -y install libselinux-python
 ansible-playbook /vagrant/ansible/site.yml -c local
